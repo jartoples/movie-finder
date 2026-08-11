e9451eb01d478f609cd8729c6391a5e92ad5d4cd
jartoples <zarofficial46@gmail.com>
2026-08-11 12:25:13 +0800

    movie finder V1

---
+++ app.py
@@ -0,0 +1,36 @@
+import requests
+from flask import Flask, render_template, request, redirect, session,jsonify
+
+from api import api_key
+
+app = Flask(__name__,template_folder=".")
+
+@app.route("/")
+def home():
+    return render_template("dashboard.html")
+@app.route("/film",methods = ["POST"])
+def film():
+    response_web = request.json
+    film = response_web["film"]
+    url = "https://api.themoviedb.org/3/search/movie"
+    params = {
+    "api_key": api_key,
+    "query": film,
+    "language": "en-US"
+}
+
+    response = requests.get(url, params=params)
+    data = response.json()
+    hasil = []
+    for film_data in data["results"]:
+        hasil.append({
+        "poster": f"https://image.tmdb.org/t/p/w154{film_data['poster_path']}",
+        "judul": film_data["title"],
+        "rating": film_data["vote_average"],
+        "overview": film_data["overview"],
+        "rilis": film_data["release_date"]
+    })
+
+    return jsonify(hasil)
+    
+app.run(debug =True)
---
+++ static/script.js
@@ -0,0 +1,34 @@
+const tombol = document.querySelector("#kirim")
+const film = document.querySelector("#film")
+const list = document.querySelector("#list")
+tombol.addEventListener("click", async function(){
+    let response = await fetch("/film",{
+        method:"POST",
+        headers:{
+            "content-type":"application/json"
+        },
+        body:JSON.stringify({
+            film:film.value
+        })
+        
+    })
+    let data = await response.json()
+    data.forEach(film => {
+        const img = document.createElement("img")
+        const judul = document.createElement("p")
+        const rating = document.createElement("p")
+        const overview = document.createElement("p")
+        const rilis = document.createElement("p")
+        img.src = film.poster
+        judul.textContent = film.judul
+        rating.textContent = film.rating
+        overview.textContent = film.overview
+        rilis.textContent = film.rilis
+        list.appendChild(img)
+        list.appendChild(judul)
+        list.appendChild(rating)
+        list.appendChild(overview)
+        list.appendChild(rilis)
+        
+    });
+})
