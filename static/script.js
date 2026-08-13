c2e01a625238cb1aa35d44ccbdda1dde256c826c
jartoples <zarofficial46@gmail.com>
2026-08-13 16:01:31 +0800

    Improve movie finder layout

--- static/script.js
+++ static/script.js
@@ -1,7 +1,8 @@
-const tombol = document.querySelector("#kirim")
+const tombol = document.querySelector("#send")
 const film = document.querySelector("#film")
 const list = document.querySelector("#list")
 tombol.addEventListener("click", async function(){
+    console.log("TOMBOL DIKLIK");
     let response = await fetch("/film",{
         method:"POST",
         headers:{
@@ -15,21 +16,33 @@ tombol.addEventListener("click", async function(){
     let data = await response.json()
     list.innerHTML = ""
     data.forEach(film => {
-        const img = document.createElement("img")
-        const judul = document.createElement("p")
-        const rating = document.createElement("p")
-        const overview = document.createElement("p")
-        const rilis = document.createElement("p")
-        img.src = film.poster
-        judul.textContent = film.judul
-        rating.textContent = film.rating
-        overview.textContent = film.overview
-        rilis.textContent = film.rilis
-        list.appendChild(img)
-        list.appendChild(judul)
-        list.appendChild(rating)
-        list.appendChild(overview)
-        list.appendChild(rilis)
+        const movie = document.createElement("div");
+        movie.classList.add("movie");
+
+        const tentang_film = document.createElement("div");
+        tentang_film.classList.add("tentang_film");
+
+        const img = document.createElement("img");
+        const judul = document.createElement("p");
+        const rating = document.createElement("p");
+        const overview = document.createElement("p");
+        const rilis = document.createElement("p");
+
+        img.src = film.poster;
+        judul.textContent = film.judul;
+        rating.textContent = film.rating + "/10";
+        overview.textContent = film.overview;
+        rilis.textContent = "release on " + film.rilis;
+
+        movie.appendChild(img);
+        movie.appendChild(tentang_film);
+
+        tentang_film.appendChild(judul);
+        tentang_film.appendChild(rating);
+        tentang_film.appendChild(overview);
+        tentang_film.appendChild(rilis);
+
+        list.appendChild(movie);
         
     });
 })
